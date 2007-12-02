3a3d08608f42ab477f5f30a1d5952a8f6adf4f4d
Ingo Korb <ingo@akana.de>
2007-12-02 19:11:24 +0100

    wrapops: Remove typedefs and de-uglify
    Using a neat gcc extension the typedefs in wrapops.h can be removed.

--- wrapops.h
+++ wrapops.h
@@ -24,6 +24,8 @@
 
    The structure-of-pgmspace-function-pointers access scheme used here
    was inspired by code from MMC2IEC by Lars Pontoppidan.
+
+   typeof-usage suggested by T. Diedrich.
 */
 
 #ifndef WRAPOPS_H
@@ -34,36 +36,31 @@
 #include "fileops.h"
 #include "tff.h"
 
-typedef void (*WRAP_READ)(char *, char *, buffer_t *);
-typedef void (*WRAP_WRITE)(char *, char *, buffer_t *, uint8_t);
-typedef uint8_t (*WRAP_DELETE)(char *, char *);
-typedef uint8_t (*WRAP_LABEL)(char *);
-typedef uint8_t (*WRAP_OPENDIR)(DIR *, char *);
-typedef int8_t  (*WRAP_READDIR)(DIR *, struct cbmdirent *);
-typedef uint16_t (*WRAP_FREEBLOCKS)(void);
-
 typedef struct {
-  WRAP_READ       open_read;
-  WRAP_WRITE      open_write;
-  WRAP_DELETE     file_delete;
-  WRAP_LABEL      disk_label;
-  WRAP_LABEL      disk_id;
-  WRAP_FREEBLOCKS disk_free;
-  WRAP_OPENDIR    opendir;
-  WRAP_READDIR    readdir;
+  void     (*open_read)(char *path, char *name, buffer_t *buf);
+  void     (*open_write)(char *path, char *name, buffer_t *buf, uint8_t append);
+  uint8_t  (*file_delete)(char *path, char *name);
+  uint8_t  (*disk_label)(char *label);
+  uint8_t  (*disk_id)(char *id);
+  uint16_t (*disk_free)(void);
+  uint8_t  (*opendir)(DIR *dh, char *path);
+  int8_t   (*readdir)(DIR *dh, struct cbmdirent *dent);
 } fileops_t;
 
 /* Pointer to the current fileops struct */
 extern const fileops_t *fop;
 
-/* Please note: This is still C, even though it may look like Lisp */
-#define open_read(dir,name,buf) (((WRAP_READ)(pgm_read_word(&(fop->open_read))))(dir,name,buf))
-#define open_write(dir,name,buf,app) (((WRAP_WRITE)(pgm_read_word(&(fop->open_write))))(dir,name,buf,app))
-#define file_delete(dir,name) (((WRAP_DELETE)(pgm_read_word(&(fop->file_delete))))(dir,name))
-#define disk_label(label) (((WRAP_LABEL)(pgm_read_word(&(fop->disk_label))))(label))
-#define disk_id(id) (((WRAP_LABEL)(pgm_read_word(&(fop->disk_id))))(id))
-#define disk_free() (((WRAP_FREEBLOCKS)(pgm_read_word(&(fop->disk_free))))())
-#define opendir(dh,dir) (((WRAP_OPENDIR)(pgm_read_word(&(fop->opendir))))(dh,dir))
-#define readdir(dh,dent) (((WRAP_READDIR)(pgm_read_word(&(fop->readdir))))(dh,dent))
+/* Helper-Define to avoid lots of typedefs */
+#define pgmcall(x) ((typeof(x))pgm_read_word(&(x)))
+
+/* Wrappers to make the indirect calls look like normal functions */
+#define open_read(dir,name,buf) ((pgmcall(fop->open_read))(dir,name,buf))
+#define open_write(dir,name,buf,app) ((pgmcall(fop->open_write))(dir,name,buf,app))
+#define file_delete(dir,name) ((pgmcall(fop->file_delete))(dir,name))
+#define disk_label(label) ((pgmcall(fop->disk_label))(label))
+#define disk_id(id) ((pgmcall(fop->disk_id))(id))
+#define disk_free() ((pgmcall(fop->disk_free))())
+#define opendir(dh,dir) ((pgmcall(fop->opendir))(dh,dir))
+#define readdir(dh,dent) ((pgmcall(fop->readdir))(dh,dent))
 
 #endif
