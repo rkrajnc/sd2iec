8b04145b538f8202a46a9ec15fe63ea0ae2de7a4
Ingo Korb <ingo@akana.de>
2007-12-24 23:51:27 +0100

    D64: Implement getlabel/getid
    Implement both getlabel and getid for D64 images and add enough
    dummy functions for testing both by loading the directory.

--- d64ops.c
+++ d64ops.c
@@ -37,23 +37,78 @@
 #include "fatops.h"
 #include "d64ops.h"
 
+#define DIRECTORY_TRACK 18
+#define LABEL_OFFSET    0x90
+#define ID_OFFSET       0xa2
+
 /* ------------------------------------------------------------------------- */
 /*  Utility functions                                                        */
 /* ------------------------------------------------------------------------- */
 
+/* Transform a track/sector pair into a D64 offset   */
+/* I wonder if the code size can be reduced further? */
+static uint32_t sector_offset(uint8_t track, const uint8_t sector) {
+  track--; /* Track numbers are 1-based */
+  if (track < 17)
+    return 256L * (track*21 + sector);
+  if (track < 24)
+    return 256L * (17*21 + (track-17)*19 + sector);
+  if (track < 30)
+    return 256L * (17*21 + 7*19 + (track-24)*18 + sector);
+  return 256L * (17*21 + 7*19 + 6*18 + (track-30)*17 + sector);
+}
+
+/* Replace oldchar with newchar in the first len bytes of buffer */
+static void strnsubst(uint8_t *buffer, uint8_t len, uint8_t oldchar, uint8_t newchar) {
+  uint8_t i = len-1;
+
+  while (i > 0) {
+    if (buffer[i] == oldchar)
+      buffer[i] = newchar;
+    i--;
+  }
+}
 /* ------------------------------------------------------------------------- */
 /*  fileops-API                                                              */
 /* ------------------------------------------------------------------------- */
+
+static uint8_t d64_opendir(dh_t *dh, char *path) {
+  return 0;
+}
+
+static int8_t d64_readdir(dh_t *dh, struct cbmdirent *dent) {
+  return -1;
+}
+
+static uint8_t d64_getlabel(char *label) {
+  if (image_read(sector_offset(DIRECTORY_TRACK,0) + LABEL_OFFSET, label, 16))
+    return 1;
+  
+  strnsubst((uint8_t *)label, 16, 0xa0, 0x20);
+  return 0;
+}
+
+static uint8_t d64_getid(char *id) {
+  if (image_read(sector_offset(DIRECTORY_TRACK,0) + ID_OFFSET, id, 5))
+    return 1;
+
+  strnsubst((uint8_t *)id, 5, 0xa0, 0x20);
+  return 0;
+}
+
+static uint16_t d64_freeblocks(void) {
+  return 0;
+}
 
 const PROGMEM fileops_t d64ops = {
   NULL, // open_read,
   NULL, // open_write,
   NULL, // delete,
-  NULL, // getlabel,
-  NULL, // getid,
-  NULL, // freeblocks,
-  NULL, // opendir,
-  NULL, // readdir,
-  image_chdir,  // mkdir...
+  d64_getlabel,
+  d64_getid,
+  d64_freeblocks,
+  d64_opendir,
+  d64_readdir,
+  image_chdir, // mkdir...
   image_chdir
 };
