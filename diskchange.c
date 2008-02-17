87ae1a7301643439719d18698178fca7ada9470e
Ingo Korb <ingo@akana.de>
2008-02-17 11:52:21 +0100

    diskchange: Remove two unneccessary initialisations
    Remove two initialisation statements from diskchange.c that just
    set some fields of a newly allocated buffer structure to zero.

--- diskchange.c
+++ diskchange.c
@@ -61,8 +61,6 @@ static void mount_line(void) {
 
   /* Grab some scratch memory - this won't fail */
   buf = alloc_buffer();
-  buf->secondary = 0;
-  buf->cleanup   = NULL;
 
   curpos = 0;
   strend = NULL;
