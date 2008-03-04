rkrajnc/sd2iec

commit 844ce652219b055d9a430e189fb98d8caac013ce

Completely rewrite path handling
This patch completely rewrites the path handling inside sd2iec.
Instead of parsing file names received from the computer into a
FAT compatible format we now base everything around path_t objects
which just hold the cluster number of a FAT directory for now.
This way the new path parser can do wildcard matching on every
directory without having to expand a path name string along the way.

Side effects include higher compatibility with the path parsing
used by the CMD drives and CMD-style paths in swap lists instead
of FAT-style paths previously.

Although not implemented by him, this patch wouldn't exist without
lots of input by Jim Brain.

doscmd.c
@@ -222,6 +222,7 @@ static void handle_memwrite(void) {
 
 static void parse_xcommand(void) {
   char *str;
+  path_t path; // FIXME: use global? Dupe in parse_command
 
   switch (command_buffer[1]) {
   case 'J':
@@ -256,7 +257,10 @@ static void parse_xcommand(void) {
 
   case 'S':
     /* Swaplist */
-    set_changelist((char *)command_buffer+3);
+    if (parse_path((char *)command_buffer+2, &path, &str, 0))
+      return;
+
+    set_changelist(&path, str);
     break;
 
 #ifdef CONFIG_STACK_TRACKING
@@ -342,6 +346,7 @@ void parse_doscommand(void) {
   uint8_t i,count;
   char *fname;
   struct cbmdirent dent;
+  path_t path;
 
   /* Set default message: Everything ok */
   set_error(ERROR_OK);
@@ -382,9 +387,8 @@ void parse_doscommand(void) {
     return;
   }
 
-  command_buffer[command_length]   = 0;
-  /* Requires less space than checks in the parsers */
-  command_buffer[command_length+1] = 0;
+  /* Clear the remainder of the command buffer, simplifies parsing */
+  memset(command_buffer+command_length, 0, sizeof(command_buffer)-command_length);
 
   /* MD/CD/RD clash with other commands, so they're checked first */
   if (command_buffer[1] == 'D') {
@@ -396,29 +400,42 @@ void parse_doscommand(void) {
 	set_error(ERROR_SYNTAX_NONAME);
 	break;
       }
-      /* Fall-through */
+      if (parse_path((char *) command_buffer+2, &path, &name, 0))
+	break;
+      mkdir(&path,name);
+      break;
 
     case 'C':
-      i = command_buffer[0];
-      parse_path((char *) command_buffer+2, (char *) command_buffer, &name);
-      if (strlen((char *) command_buffer) != 0) {
-	/* Join path and name */
-	name[-1] = '/';
-	name = (char *) command_buffer;
-      } else
-	/* Yay, special case: CD/name/ means ./name   */
-	/* Technically the terminating / is required, */
-	/* but who'll notice if we don't check?       */
-	if (name[0] == '/')
-	  name++;
+      if (parse_path((char *) command_buffer+2, &path, &name, 1))
+	break;
+
+      if (strlen(name) != 0) {
+        /* Path component after the : */
+        if (name[0] == '_') {
+          /* Going up a level - let chdir handle it. */
+          chdir(&path,name);
+        } else {
+          /* A directory name - try to match it */
+          if (first_match(&path, name, FLAG_HIDDEN, &dent))
+            break;
+
+          /* Move into it if it's a directory, use chdir if it's a file. */
+          if ((dent.typeflags & TYPE_MASK) != TYPE_DIR)
+            chdir(&path,(char *)dent.name);
+          else
+            current_dir = dent.path;
+        }
+      } else {
+        if (current_dir.fat == path.fat) {
+          set_error(ERROR_FILE_NOT_FOUND_39);
+          break;
+        } else {
+          current_dir = path;
+        }
+      }
 
       if (iecflags.autoswap_active)
-	set_changelist(NULLSTRING);
-
-      if (i == 'C')
-	chdir(name);
-      else
-	mkdir(name);
+        set_changelist(NULL, NULLSTRING);
 
       break;
 
@@ -442,7 +459,7 @@ void parse_doscommand(void) {
       if (command_buffer[i] != ':') {
 	set_error(ERROR_SYNTAX_NONAME);
       } else {
-	i = file_delete(NULL, (char *)command_buffer+i+1);
+	i = file_delete(&current_dir, (char *)command_buffer+i+1);
 	if (i != 255)
 	  set_error_ts(ERROR_SCRATCHED,i,0);
       }
@@ -588,9 +605,9 @@ void parse_doscommand(void) {
 
   case 'S':
     /* Scratch */
-    parse_path((char *) command_buffer+1, (char *) command_buffer, &fname);
+    parse_path((char *) command_buffer+1, &path, &fname, 0);
 
-    if (opendir(&matchdh, (char *) command_buffer))
+    if (opendir(&matchdh, &path))
       return;
 
     i = 255;
@@ -599,7 +616,7 @@ void parse_doscommand(void) {
       /* Skip directories */
       if ((dent.typeflags & TYPE_MASK) == TYPE_DIR)
 	continue;
-      i = file_delete(NULL, (char *)dent.name);
+      i = file_delete(&path, (char *)dent.name);
       if (i != 255)
 	count += i;
       else

README
@@ -41,11 +41,16 @@ Supported commands:
 
   Quick syntax overview:
     CD:_         changes into the parent dir (_ is the left arrow on the C64)
+    CD_          dito
     CD:foo       changes into foo
     CD/foo       dito
     CD//foo      changes into \foo
     CD/foo/:bar  changes into foo\bar
     CD/foo/bar   dito
+
+  You can use wildcards anywhere in the path. To change into an M2I or D64
+  image the image file must be named after the :, it will not be recognized
+  otherwise.
 
   MD uses a syntax similiar to CD and will create the directory listed
   after the colon (:) relative to any directory listed before it.
@@ -212,12 +217,13 @@ add it yourself:
   to find out how to connect it.
 
 To use this functionality, create a text file that lists the file
-names of all disk images you want to swap between, one per line. The
-file names are not parsed in any way, so you can include FAT paths of
-the disk images if you use / instead of \. After that you can set up
-the swap list by sending "XS:filename" over the command channel with
-filename being the name of the image list you created (again,
-unparsed).
+names of all disk images you want to swap between, one per line.
+The file names are parsed in the same way as the CD command, so you
+can include a path to the image if desired.
+
+The swap list is enabled by sending "XS:filename" over the command channel
+with filename being the name of the image list you created, parsed
+in the same way as any other file name.
 
 After sending XS the first image in the list is automatically
 mounted. To switch to the next image in the list, push the button. If
@@ -230,7 +236,7 @@ button.
 
 Due to the way this feature is implemented you are not limited to a
 swap list containing just D64 images, M2I and even FAT directories
-will work too.
+will work too.  FIXME: Does that still work?
 
 If you press the button when no list has been set before or when the
 previous list was cleared by sending XS the software will look for a

d64ops.c
@@ -515,7 +515,7 @@ static uint8_t d64_write_cleanup(buffer_t *buf) {
 /*  fileops-API                                                              */
 /* ------------------------------------------------------------------------- */
 
-static uint8_t d64_opendir(dh_t *dh, char *path) {
+static uint8_t d64_opendir(dh_t *dh, path_t *path) {
   dh->d64.track  = DIR_TRACK;
   dh->d64.sector = DIR_START_SECTOR;
   dh->d64.entry  = 0;
@@ -576,7 +576,7 @@ static uint16_t d64_freeblocks(void) {
   return blocks;
 }
 
-static void d64_open_read(char *path, char *name, buffer_t *buf) {
+static void d64_open_read(path_t *path, char *name, buffer_t *buf) {
   /* WARNING: Ugly hack used here. The directory entry is still in  */
   /*          entrybuf because of match_entry in fatops.c/file_open */
   buf->data[0] = entrybuf[OFS_TRACK];
@@ -590,7 +590,7 @@ static void d64_open_read(char *path, char *name, buffer_t *buf) {
   buf->refill(buf);
 }
 
-static void d64_open_write(char *path, char *name, uint8_t type, buffer_t *buf, uint8_t append) {
+static void d64_open_write(path_t *path, char *name, uint8_t type, buffer_t *buf, uint8_t append) {
   dh_t dh;
   int8_t res;
   uint8_t t,s;
@@ -720,7 +720,7 @@ static void d64_open_write(char *path, char *name, uint8_t type, buffer_t *buf, 
   active_buffers += 16;
 }
 
-static uint8_t d64_delete(char *path, char *name) {
+static uint8_t d64_delete(path_t *path, char *name) {
   /* At this point entrybuf will contain the directory entry and    */
   /* matchdh will almost point to it (entry incremented in readdir) */
   buffer_t *buf;

diskchange.c
@@ -31,8 +31,9 @@
 #include "buffers.h"
 #include "errormsg.h"
 #include "fatops.h"
-#include "iec.h"
 #include "ff.h"
+#include "iec.h"
+#include "parser.h"
 #include "diskchange.h"
 
 static const char PROGMEM autoswap_name[] = "AUTOSWAP.LST";
@@ -40,6 +41,7 @@ static const char PROGMEM autoswap_name[] = "AUTOSWAP.LST";
 volatile uint8_t keycounter;
 
 static FIL     swaplist;
+static path_t  swappath;
 static uint8_t linenum;
 
 /* Timer-polling delay */
@@ -111,8 +113,18 @@ static void mount_line(void) {
   if (fop != &fatops)
     image_unmount();
 
+  /* Parse the path */
+  path_t path;
+  char *name; // FIXME: Remove this variable, reuse str instead
+
+  /* Start in the directory of the swap list */
+  current_dir = swappath;
+
+  if (parse_path((char *) buf->data, &path, &name, 0))
+    return;
+
   /* Mount the disk image */
-  fat_chdir((char *)buf->data);
+  fat_chdir(&path, name);
 
   free_buffer(buf);
 
@@ -132,7 +144,7 @@ static void mount_line(void) {
   while (keycounter == DISKCHANGE_MAX) ;
 }
 
-void set_changelist(char *filename) {
+void set_changelist(path_t *path, char *filename) {
   FRESULT res;
 
   /* Assume this isn't the auto-swap list */
@@ -145,12 +157,19 @@ void set_changelist(char *filename) {
     linenum = 255;
   }
 
+  if (strlen(filename) == 0)
+    return;
+
   /* Open a new swaplist */
+  fatfs.curr_dir = path->fat;
   res = f_open(&swaplist, filename, FA_READ | FA_OPEN_EXISTING);
   if (res != FR_OK) {
     parse_error(res,1);
     return;
   }
+
+  /* Remember its directory so relative paths work */
+  swappath = *path;
 
   linenum = 0;
   mount_line();
@@ -162,7 +181,7 @@ void change_disk(void) {
     /* No swaplist active, try using AUTOSWAP.LST */
     /* change_disk is called from the IEC idle loop, so entrybuf is free */
     strcpy_P((char *)entrybuf, autoswap_name);
-    set_changelist((char *)entrybuf);
+    set_changelist(&current_dir, (char *)entrybuf);
     if (linenum == 255) {
       /* No swap list found, clear error and exit */
       set_error(ERROR_OK);

diskchange.h
@@ -24,10 +24,15 @@
 
 */
 
+#ifndef DISKCHANGE_H
+#define DISKCHANGE_H
+
+#include "dirent.h"
+
 void init_change(void);
 void change_disk(void);
-void set_changelist(char *filename);
+void set_changelist(path_t *path, char *filename);
 
 extern volatile uint8_t keycounter;
 
-
+#endif

fatops.c
@@ -36,6 +36,7 @@
 #include "ff.h"
 #include "fileops.h"
 #include "m2iops.h"
+#include "parser.h"
 #include "uart.h"
 #include "wrapops.h"
 #include "fatops.h"
@@ -180,21 +181,6 @@ static void pet2asc(char *buf) {
   }
 }
 
-static char* build_name(char *path, char *name, buffer_t *buf) {
-  char *str;
-
-  if (path && strlen(path)) {
-    str = (char *) buf->data;
-    strcpy(str, path);
-    strcat_P(str, PSTR("/"));
-    strcat(str, name);
-  } else
-    str = name;
-
-  pet2asc(str);
-  return str;
-}
-
 /* ------------------------------------------------------------------------- */
 /*  Callbacks                                                                */
 /* ------------------------------------------------------------------------- */
@@ -316,10 +302,11 @@ static uint8_t fat_file_close(buffer_t *buf) {
  * This functions opens a file in the FAT filesystem for reading and sets up
  * buf to access it.
  */
-void fat_open_read(char *path, char *filename, buffer_t *buf) {
+void fat_open_read(path_t *path, char *filename, buffer_t *buf) {
   FRESULT res;
 
-  filename = build_name(path, filename, buf);
+  pet2asc(filename);
+  fatfs.curr_dir = path->fat;
   res = f_open(&buf->pvt.fh, filename, FA_READ | FA_OPEN_EXISTING);
   if (res != FR_OK) {
     parse_error(res,1);
@@ -347,10 +334,11 @@ void fat_open_read(char *path, char *filename, buffer_t *buf) {
  * buf to access it. type is ignored here because FAT has no equivalent of
  * file types.
  */
-void fat_open_write(char *path, char *filename, uint8_t type, buffer_t *buf, uint8_t append) {
+void fat_open_write(path_t *path, char *filename, uint8_t type, buffer_t *buf, uint8_t append) {
   FRESULT res;
 
-  filename = build_name(path, filename, buf);
+  pet2asc(filename);
+  fatfs.curr_dir = path->fat;
   if (append) {
     res = f_open(&buf->pvt.fh, filename, FA_WRITE | FA_OPEN_EXISTING);
     if (res == FR_OK)
@@ -376,11 +364,10 @@ void fat_open_write(char *path, char *filename, uint8_t type, buffer_t *buf, uin
 /*  External interface for the various operations                            */
 /* ------------------------------------------------------------------------- */
 
-uint8_t fat_opendir(dh_t *dh, char *dir) {
+uint8_t fat_opendir(dh_t *dh, path_t *path) {
   FRESULT res;
 
-  pet2asc(dir);
-  res = f_opendir(&dh->fat, dir);
+  res = l_opendir(&fatfs, path->fat, &dh->fat);
   if (res != FR_OK) {
     parse_error(res,1);
     return 1;
@@ -470,19 +457,16 @@ int8_t fat_readdir(dh_t *dh, struct cbmdirent *dent) {
  * This function deletes the file filename in path and returns
  * 0 if not found, 1 if deleted or 255 if an error occured.
  */
-uint8_t fat_delete(char *path, char *filename) {
-  buffer_t *buf;
+uint8_t fat_delete(path_t *path, char *filename) {
   FRESULT res;
 
-  buf = alloc_buffer();
-  if (!buf)
-    return 255;
-
   DIRTY_LED_ON();
-  filename = build_name(path, filename, buf);
+  pet2asc(filename);
+  fatfs.curr_dir = path->fat;
   res = f_unlink(filename);
-  /* free_buffer will turn off the LED for us */
-  free_buffer(buf);
+
+  if (!(active_buffers & 0xf0))
+    DIRTY_LED_OFF();
 
   parse_error(res,0);
   if (res == FR_OK)
@@ -502,46 +486,40 @@ uint8_t fat_delete(char *path, char *filename) {
  * current directory will be changed to the directory of the file and
  * it will be mounted as an image file.
  */
-void fat_chdir(char *dirname) {
+void fat_chdir(path_t *path, char *dirname) {
   FRESULT res;
+  FILINFO finfo;
+
+  fatfs.curr_dir = path->fat;
 
   /* Left arrow moves one directory up */
   if (dirname[0] == '_' && dirname[1] == 0) {
-    dirname[0] = '.';
-    dirname[1] = '.';
-    dirname[2] = 0;
-  } else {
-    pet2asc(dirname);
-    if (dirname[strlen(dirname)-1] == '/')
-      dirname[strlen(dirname)-1] = 0;
+    command_buffer[0] = '.';
+    command_buffer[1] = '.';
+    command_buffer[2] = 0;
+    dirname = (char *)command_buffer;
   }
-  res = f_chdir(dirname);
-  if (res == FR_NOT_DIRECTORY) {
+
+  pet2asc(dirname);
+  res = f_stat(dirname, &finfo);
+  if (res != FR_OK) {
+    parse_error(res,1);
+    return;
+  }
+
+  if (finfo.fattrib & AM_DIR) {
+    /* It's a directory, change to its cluster */
+    current_dir.fat = finfo.clust;
+  } else {
     /* Changing into a file, could be a mount request */
-    char *ext;
-    char *fname = strrchr(dirname, '/');
-
-    if (fname)
-      *fname++ = 0;
-    else
-      fname = dirname;
-
-    ext = strrchr(fname, '.');
+    char *ext = strrchr(dirname, '.');
 
     if (ext && (!strcasecmp_P(ext, PSTR(".m2i")) ||
 		!strcasecmp_P(ext, PSTR(".d64")))) {
       /* D64/M2I mount request */
-      if (fname != dirname) {
-	res = f_chdir(dirname);
-	if (res != FR_OK) {
-	  parse_error(res,1);
-	  return;
-	}
-      }
-
       free_all_buffers(1);
       /* Open image file */
-      res = f_open(&imagehandle, fname, FA_OPEN_EXISTING|FA_READ|FA_WRITE);
+      res = f_open(&imagehandle, dirname, FA_OPEN_EXISTING|FA_READ|FA_WRITE);
       if (res != FR_OK) {
 	parse_error(res,1);
 	return;
@@ -552,16 +530,17 @@ void fat_chdir(char *dirname) {
       else
 	fop = &d64ops;
 
+      current_dir.fat = fatfs.curr_dir;
       return;
     }
   }
-  parse_error(res,1);
 }
 
 /* Create a new directory */
-void fat_mkdir(char *dirname) {
+void fat_mkdir(path_t *path, char *dirname) {
   FRESULT res;
 
+  fatfs.curr_dir = path->fat;
   pet2asc(dirname);
   res = f_mkdir(dirname);
   parse_error(res,0);
@@ -580,13 +559,10 @@ uint8_t fat_getlabel(char *label) {
   FILINFO finfo;
   FRESULT res;
   uint8_t i,j;
-  char rootdir[2];
 
   memset(label, ' ', 16);
 
-  rootdir[0] = '/';
-  rootdir[1] = 0;
-  res = f_opendir(&dh, rootdir);
+  res = l_opendir(&fatfs, 0, &dh);
 
   if (res != FR_OK) {
     parse_error(res,0);
@@ -682,8 +658,11 @@ void init_fatops(void) {
   fop = &fatops;
   f_mount(0, &fatfs);
 
+  /* Reset the current path */
+  current_dir.fat = 0;
+
   /* Dummy operation to force the actual mounting */
-  f_chdir(NULLSTRING);
+  f_open(&imagehandle, NULLSTRING, FA_OPEN_EXISTING);
 }
 
 /**
@@ -712,7 +691,7 @@ void image_unmount(void) {
  * chdir/mkdir for all image types that don't support subdirectories
  * themselves.
  */
-void image_chdir(char *dirname) {
+void image_chdir(path_t *path, char *dirname) {
   if (dirname[0] == '_' && dirname[1] == 0) {
     /* Unmount request */
     image_unmount();

fatops.h
@@ -28,20 +28,24 @@
 #define FATOPS_H
 
 #include "buffers.h"
+#include "dirent.h"
 #include "wrapops.h"
+#include "ff.h"
+
+extern FATFS fatfs;
 
 /* API */
 void     init_fatops(void);
 void     parse_error(FRESULT res, uint8_t readflag);
-uint8_t  fat_delete(char *path, char *filename);
-void     fat_chdir(char *dirname);
-void     fat_mkdir(char *dirname);
-void     fat_open_read(char *path, char *filename, buffer_t *buf);
-void     fat_open_write(char *path, char *filename, uint8_t type, buffer_t *buf, uint8_t append);
+uint8_t  fat_delete(path_t *path, char *filename);
+void     fat_chdir(path_t *path, char *dirname);
+void     fat_mkdir(path_t *path, char *dirname);
+void     fat_open_read(path_t *path, char *filename, buffer_t *buf);
+void     fat_open_write(path_t *path, char *filename, uint8_t type, buffer_t *buf, uint8_t append);
 uint8_t  fat_getlabel(char *label);
 uint8_t  fat_getid(char *id);
 uint16_t fat_freeblocks(void);
-uint8_t  fat_opendir(dh_t *dh, char *dir);
+uint8_t  fat_opendir(dh_t *dh, path_t *dir);
 int8_t   fat_readdir(dh_t *dh, struct cbmdirent *dent);
 void     fat_sectordummy(buffer_t *buf, uint8_t track, uint8_t sector);
 
@@ -49,7 +53,7 @@ extern const fileops_t fatops;
 
 /* Generic helpers */
 void    image_unmount(void);
-void    image_chdir(char *dirname);
+void    image_chdir(path_t *path, char *dirname);
 uint8_t image_read(DWORD offset, void *buffer, uint16_t bytes);
 uint8_t image_write(DWORD offset, void *buffer, uint16_t bytes, uint8_t flush);
 

ff.c
@@ -1794,6 +1794,47 @@ FRESULT f_opendir (
 
 
 
+/**
+ * l_opendir - open a directory by cluster number
+ * @fs     : Pointer to the FATFS structure of the target file system
+ * @dj     : Pointer to the DIR structure to be filled
+ * @cluster: Number of the start cluster of the directory (0=root)
+ *
+ * This functions works like f_opendir, but instead of a path the directory
+ * to be opened is specified by the FATFS structure and the starting cluster
+ * number. Use 0 for the cluster to open the root directory.
+ * Always returns FR_OK.
+ */
+FRESULT l_opendir(FATFS* fs, DWORD cluster, DIR *dj) {
+  dj->fs = fs;
+  //dj->id = fs->id;
+
+  if (cluster == 0) {
+	/* Open the root directory */
+    cluster = fs->dirbase;
+    if (fs->fs_type == FS_FAT32) {
+      dj->clust = dj->sclust = cluster;
+      dj->sect  = clust2sect(fs, cluster);
+    } else {
+      dj->clust = dj->sclust = 0;
+      dj->sect  = cluster;
+    }
+    dj->index = 0;
+  } else {
+	/* Open a non-root directory */
+    dj->clust = dj->sclust = cluster;
+    dj->sect  = clust2sect(fs, cluster);
+#if _USE_CHDIR != 0
+    dj->index = 0;
+#else
+    dj->index = 2;
+#endif
+  }
+  return FR_OK;
+}
+
+
+
 
 /*-----------------------------------------------------------------------*/
 /* Read Directory Entry in Sequense                                      */

commit feb3e15717007cfe49fa24d83c2b54e3afb2a8eb

doscmd: Do not overwrite the filename sent by Turbodisk
The Turbodisk support relies on the fact that command_buffer is
not completely cleared between commands. One of the changes in
844ce652 broke that assumption by clearing all unused bytes of the
buffer, fix the problem by not doing that when Turbodisk was
detected.

doscmd.c
@@ -388,8 +388,14 @@ void parse_doscommand(void) {
     return;
   }
 
-  /* Clear the remainder of the command buffer, simplifies parsing */
-  memset(command_buffer+command_length, 0, sizeof(command_buffer)-command_length);
+#ifdef CONFIG_TURBODISK
+  if (detected_loader == FL_TURBODISK) {
+    /* Don't overwrite the file name in the final M-W command of Turbodisk */
+    command_buffer[command_length] = 0;
+  } else
+#endif
+    /* Clear the remainder of the command buffer, simplifies parsing */
+    memset(command_buffer+command_length, 0, sizeof(command_buffer)-command_length);
 
   /* MD/CD/RD clash with other commands, so they're checked first */
   if (command_buffer[1] == 'D') {