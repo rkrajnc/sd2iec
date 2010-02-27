rkrajnc/sd2iec

commit bdaec170060d19d786ceca1feb150eac22fbc83a

Fix description and implementation of P command for non-REL
Fix the description of the P command (previously big-endian, now
little-endian) and implementation of the P command (previously
parsed as mid/hi/lo) for file types other than REL.

doscmd.c
@@ -980,25 +980,12 @@ static void parse_new(void) {
 /* -------------- */
 static void parse_position(void) {
   buffer_t *buf;
-  uint8_t hi = 0, lo, pos;
+
+  clean_cmdbuffer();
 
   if(command_length < 2 || (buf = find_buffer(command_buffer[1] & 0x0f)) == NULL) {
     set_error(ERROR_NO_CHANNEL);
     return;
-  }
-
-  lo = pos = (buf->recordlen? 1:0); // REL file start indexes at one.
-
-  if(command_length > 1)
-    lo = command_buffer[2];
-  if(command_length > 2)
-    hi = command_buffer[3];
-  if(command_length > 3) {
-    pos = command_buffer[4];
-    if(buf->recordlen && pos >= buf->recordlen) {
-      set_error(ERROR_RECORD_OVERFLOW);
-      return;
-    }
   }
 
   if (buf->seek == NULL) {
@@ -1006,11 +993,44 @@ static void parse_position(void) {
     return;
   }
 
-  if(buf->recordlen) { // REL files start indexes at 1.
+  if (buf->recordlen) {
+    /* REL file */
+    uint8_t hi, lo, pos;
+    hi  = 0;
+    lo  = 1;
+    pos = 1;
+
+    if (command_length > 1)
+      lo = command_buffer[2];
+    if (command_length > 2)
+      hi = command_buffer[3];
+    if (command_length > 3)
+      pos = command_buffer[4];
+
+    if (pos >= buf->recordlen) {
+      set_error(ERROR_RECORD_OVERFLOW);
+      return;
+    }
+
     lo--;
     pos--;
+    buf->seek(buf, (hi * 256UL + lo) * (uint32_t)buf->recordlen * pos, pos);
+  } else {
+    /* Non-REL seek uses a straight little-endian offset */
+    union {
+      uint32_t l;
+      uint8_t  c[4];
+    } offset;
+
+    // smaller than memcpy
+    /* WARNING: Endian-dependant */
+    offset.c[0] = command_buffer[2];
+    offset.c[1] = command_buffer[3];
+    offset.c[2] = command_buffer[4];
+    offset.c[3] = command_buffer[5];
+
+    buf->seek(buf, offset.l, 0);
   }
-  buf->seek(buf,(hi * 256UL + lo) * (uint32_t)(buf->recordlen ? buf->recordlen : 256),pos);
 }
 
 

README
@@ -189,8 +189,10 @@ Supported commands:
 - P
   Positioning doesn't just work for REL files but also for regular
   files on a FAT partition. When used for regular files the format
-  is "P"+chr$(channel)+chr$(hi)+chr$(mid)+chr$(lo) which will seek
-  to the 0-based offset hi*65536+256*mid+lo in the file.
+  is "P"+chr$(channel)+chr$(lo)+chr$(midlo)+chr$(midhi)+chr$(hi)
+  which will seek to the 0-based offset hi*2^24+midhi*65536+256*midlo+lo
+  in the file. If you send less than four bytes for the offset, the
+  missing bytes are assumed to be zero.
 
 - N:
   Format works only if a D64 image is already mounted. This command is

commit 77b11143f95d4957688d0711d84b4e33fe9d590f

Fix a few problems in parse_position affecting REL files
parse_position had a few issues, some of the introduced in the
changes in bdaec170:
- position byte was incorrectly multiplied into the offset
- position of 13 was removed due to clean_cmdbuffer()
- record calculation was incorrect for records 256*n
  because only the low byte was adjusted

doscmd.c
@@ -78,7 +78,7 @@ uint8_t globalflags;
 #endif
 
 uint8_t command_buffer[CONFIG_COMMAND_BUFFER_SIZE+2];
-uint8_t command_length;
+uint8_t command_length,original_length;
 
 date_t date_match_start;
 date_t date_match_end;
@@ -991,6 +991,7 @@ static void parse_new(void) {
 static void parse_position(void) {
   buffer_t *buf;
 
+  command_length = original_length;
   clean_cmdbuffer();
 
   if(command_length < 2 || (buf = find_buffer(command_buffer[1] & 0x0f)) == NULL) {
@@ -1005,26 +1006,30 @@ static void parse_position(void) {
 
   if (buf->recordlen) {
     /* REL file */
-    uint8_t hi, lo, pos;
-    hi  = 0;
-    lo  = 1;
+    uint16_t record;
+    uint8_t  pos;
+    record = 1;
     pos = 1;
 
     if (command_length > 1)
-      lo = command_buffer[2];
+      record = command_buffer[2];
     if (command_length > 2)
-      hi = command_buffer[3];
+      record |= command_buffer[3] << 8;
     if (command_length > 3)
       pos = command_buffer[4];
 
-    if (pos >= buf->recordlen) {
+    if (pos > buf->recordlen) {
       set_error(ERROR_RECORD_OVERFLOW);
       return;
     }
 
-    lo--;
-    pos--;
-    buf->seek(buf, (hi * 256UL + lo) * (uint32_t)buf->recordlen * pos, pos);
+    if (record)
+      record--;
+
+    if (pos)
+      pos--;
+
+    buf->seek(buf, record * (uint32_t)buf->recordlen, pos);
   } else {
     /* Non-REL seek uses a straight little-endian offset */
     union {
@@ -1609,6 +1614,7 @@ void parse_doscommand(void) {
 #endif
 
   /* Remove one CR at end of command */
+  original_length = command_length;
   if (command_length > 0 && command_buffer[command_length-1] == 0x0d)
     command_length--;
 