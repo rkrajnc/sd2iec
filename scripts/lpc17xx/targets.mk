4d46c62225d7ca25bfc252948725b9e805bcadf7
Ingo Korb <ingo@akana.de>
2012-02-10 20:49:46 +0100

    lpc17xx: Add vector checksum to generated binary
    Calculate the correct vector checksum to the .bin file generated for
    lpc17xx builds to make sure the binary is accepted by the ROM
    bootloader of the chip.

--- scripts/lpc17xx/targets.mk
+++ scripts/lpc17xx/targets.mk
@@ -3,3 +3,9 @@
 # A little helper target for the maintainer =)
 copy:
 	cp $(TARGET).bin /mbed
+
+ifneq ($(CONFIG_BOOTLOADER),y)
+bin:
+	$(E) "  CHKSUM $(TARGET).bin"
+	$(Q)scripts/lpc17xx/lpc_checksum.pl $(TARGET).bin
+endif
